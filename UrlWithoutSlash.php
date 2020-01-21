09db6764e34bbe4b3c8b3b4003235239e5a89f80
Artem Surkov <artsur@mail.ru>
2020-01-21 12:33:56 +0300

    Fix bug on main page

--- UrlWithoutSlash.php
+++ UrlWithoutSlash.php
@@ -16,16 +16,14 @@ class UrlWithoutSlash
      */
     public function handle($request, Closure $next)
     {
-
-        if(mb_substr($request->getPathInfo(),-1)=='/' && ($uri = $request->getRequestUri())!='/'){
+        if(mb_substr($request->getPathInfo(),-1)=='/' and $request->getPathInfo()!='/' and ($uri = $request->getRequestUri())!='/'){
             if(mb_substr($uri,0,mb_stripos($uri,'?')) != ''){
-                $new_uri = mb_substr($uri,0,mb_stripos($uri,'?')-1).mb_substr($uri,mb_stripos($uri,'?'));
+                $new_uri = mb_substr($request->getPathInfo(),0,-1).'?'.$request->getQueryString();
             }else{
                 $new_uri = mb_substr($request->getPathInfo(),0,-1);
             }
             return redirect(env('APP_URL').$new_uri,301);
         }
-
         return $next($request);
     }
 }
